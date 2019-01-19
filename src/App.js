b3c94d3c2a2ee5f970e01484e73f7013553f4ebe
Xiaohuan Li <xiaohuan.li@gmail.com>
2019-01-19 00:14:29 +0000

    green

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Card colorSelected="purple" count={0} onType={()=>{console.log("typed");}}/>
+        <Card colorSelected="green" count={0} onType={()=>{console.log("typed");}}/>
       </div>
     );
   }
